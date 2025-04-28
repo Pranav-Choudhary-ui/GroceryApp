bafb4a5056c25249ef59962634bf13803037eccb
Pranav Choudhary <pranavsingh4703@gmail.com>
2025-04-28 13:11:44 +0530

    Added the Recycler View and its adapters

--- app/src/main/java/com/example/groceryapp/ItemAdapter.java
+++ app/src/main/java/com/example/groceryapp/ItemAdapter.java
@@ -1,27 +1,60 @@
 package com.example.groceryapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ItemAdapter extends RecyclerView.Adapter<>{
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.MyViewHolder>{
 
-    ArrayList<Item> itemArrayList;
-    Context context;
+    List<Item> itemList;
 
-    public ItemAdapter(ArrayList<Item> itemArrayList, Context context) {
-        super(context, R.layout.item_layout, itemArrayList);
-        this.itemArrayList = itemArrayList;
-        this.context = context;
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,parent,false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Item item = itemList.get(position);
+
+        holder.title.setText(item.getItem_title());
+        holder.desc.setText(item.getDescription());
+        holder.imgView.setImageResource(item.getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public ItemAdapter(List<Item> itemArrayList) {
+        this.itemList = itemArrayList;
     }
     public static class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView imgView;
+        TextView title;
+        TextView desc;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imgView = itemView.findViewById(R.id.item_img);
+            title = itemView.findViewById(R.id.item_title);
+            desc = itemView.findViewById(R.id.item_desc);
+
         }
     }
 }
--- app/src/main/java/com/example/groceryapp/MainActivity.java
+++ app/src/main/java/com/example/groceryapp/MainActivity.java
@@ -7,8 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Item> itemList;
+    ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        itemList = new ArrayList<>();
+        Item item1 = new Item("Fruits","Fresh Fruits from the Garden",R.drawable.fruit);
+        Item item2 = new Item( "Vegetables","Delicious Vegetables ",R.drawable.vegitables);
+        Item item3 = new Item("Bakery","Bread, Wheat and Beans",R.drawable.bread);
+        Item item4 = new Item( "Beverage","Juice, Tea, Coffee and Soda",R.drawable.beverage);
+        Item item5 = new Item( "Milk", "Milk, Shakes and Yogurt",R.drawable.milk);
+        Item item6 = new Item("Snacks","Pop Corn, Donut and Drinks",R.drawable.popcorn);
+
+
+        itemList.add(item1);
+        itemList.add(item2);
+        itemList.add(item3);
+        itemList.add(item4);
+        itemList.add(item5);
+        itemList.add(item6);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+//        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+//        if (recyclerView != null) {
+//            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//            recyclerView.setLayoutManager(layoutManager);
+//        }
+
+
+        itemAdapter = new ItemAdapter(itemList);
+        recyclerView.setAdapter(itemAdapter);
     }
 }
